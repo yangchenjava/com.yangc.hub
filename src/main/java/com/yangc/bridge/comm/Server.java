278f1c0906f236b0a7d8edf501b20cd207a05ece
yangchenjava <yangchen_java@126.com>
2016-04-26 10:17:17 +0800

    com.yangc.hub

--- src/main/java/com/yangc/bridge/comm/Server.java
+++ src/main/java/com/yangc/bridge/comm/Server.java
@@ -21,7 +21,7 @@ import java.util.Map.Entry;
 
 import javax.net.ssl.SSLEngine;
 
-import org.apache.commons.collections.MapUtils;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/com/yangc/bridge/comm/handler/processor/LoginProcessor.java
+++ src/main/java/com/yangc/bridge/comm/handler/processor/LoginProcessor.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.jms.JMSException;
 import javax.jms.ObjectMessage;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
